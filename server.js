432386203deff380e099785423ff9b6bf832b7cc
Sivakumar Arivazhagan <SivakumarA@VISUALBI.LOCAL>
2019-02-19 13:40:49 +0530

    deploy commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http =require('http');
 const express = require('express');
 const app=express();
 const port = process.env.PORT || 3000;
 const  mongoose = require('mongoose');
 var result = require('./schema');
 
+
 //server start
 app.listen(port,() =>{
-    console.log("Started")
+    console.log("Started");
+
 });
 
 app.use(function(req, res, next) {
@@ -16,19 +17,20 @@ app.use(function(req, res, next) {
     next();
   });
 //mongo DB connection
-const url = 'mongodb://localhost/test_report';
+const url ='mongodb://10.0.1.112/test_report';
 mongoose.connect(url, {useNewUrlParser: true});
 mongoose.connection.on('connected', () => console.log("mongodb connected"));
 mongoose.connection.on('error', (err) => console.log(err));
 
 
-// http://localhost:3000/getall
+
+// http://10.0.1.112:3000/getall
 // get all reports
 app.get('/getall',(req,res,next) => {
 
     result.find({}, (err, data) => {
         if(err) res.status(500).send(err);
-        if(data) res.status(200).send(data);
+        if(data) res.status(200).json(data);
     });
 });
 
@@ -42,3 +44,19 @@ app.get("/report",(req,res,next) =>{
     })
 });
 
+app.get("/todaySummary",(req,res,next) =>{
+    var date = new Date().toISOString().slice(0,10);
+    console.log(date);
+result.find({'testCases.testCaseStart':new RegExp(".*"+date+".*", "i")},(err,data) => {
+        if(err) res.status(500).send(err)
+        if(data) res.status(200).send({data})
+    });
+});
+
+app.get("/lastsummary",(req,res,next) =>{
+    result.find({}).sort({_id: -1}).limit(5).exec(function (err, data) {
+        if(err) res.status(500).send(err)
+        if(data) res.status(200).send({data})
+    });
+      
+});
